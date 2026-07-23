b285b9931bb8b434607434cbf96ab254f33af1c3
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-24 00:26:18 +0300

    fixed: lint

--- hello_flutter/lib/button.dart
+++ hello_flutter/lib/button.dart
@@ -1,3 +1,36 @@
+// import 'package:flutter/material.dart';
+
+// class MyButton extends StatelessWidget {
+//   const MyButton({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         print('MyButton was tapped');
+//       },
+//       child: Container(
+//         height: 50,
+//         padding: const EdgeInsets.all(8),
+//         margin: const EdgeInsets.symmetric(horizontal: 8),
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(5),
+//           color: Colors.lightGreen,
+//         ),
+//         child: const Center(child: Text('Engage')),
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Scaffold(body: Center(child: MyButton())),
+//     ),
+//   );
+// }
+
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
@@ -11,7 +44,7 @@ class MyButton extends StatelessWidget {
       },
       child: Container(
         height: 50,
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(5),
         margin: const EdgeInsets.symmetric(horizontal: 8),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5),
@@ -27,6 +60,6 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(body: Center(child: MyButton())),
-    ),
+    )
   );
 }
