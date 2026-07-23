09fc1c335e734372f9b00cbb348d2c24c46e1e07
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-24 00:35:52 +0300

    fixed: lint

---
+++ hello_flutter/lib/statefull.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  // This class is the configuration for the state
+  // It holds the values (in this case nothing) provided
+  // State, Fields in awidget subclass are always marked
+  // "final".
+
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class 
