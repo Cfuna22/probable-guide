6d0f48b5afe5b95ab32cee60cda7051fb27bef98
Abel Sifuna <abelsifuna515@gmail.com>
2026-07-24 00:46:33 +0300

    fixed: lint

--- hello_flutter/lib/statefull.dart
+++ hello_flutter/lib/statefull.dart
@@ -12,4 +12,30 @@ class Counter extends StatefulWidget {
   State<Counter> createState() => _CounterState();
 }
 
-class 
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // This calls to setState tells the Flutter framework
+      // that something has changed in the state, which
+      // causes it to return the build method below so that
+      // the display can reflect the updated values, if you
+      // change _counter without calling setState(), then
+      // the build method won't be called again, and so
+      // nothing would appear to happen
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun everytime setState is called,
+    // for instance, as done by the _increment method above.
+    // The flutter framework has been optimized to make
+    // rerunning build methods fast, so that you can just
+    // rebuild anything that needs updating rather that
+    // having to individually change instances of widgets.
+    return Row();
+  }
+}
